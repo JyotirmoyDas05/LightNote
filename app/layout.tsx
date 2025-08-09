b1412943636d19eb3a0a0019613dc75efb355b87
Jyotirmoy Das <jyotimoydas12345@gmail.com>
2025-08-09 08:14:15 +0530

    chore: update @tiptap/extension-collaboration-cursor to version 3.0.0 and
    - add @vercel/analytics dependency

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
+import { Analytics } from '@vercel/analytics/next';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -48,6 +49,7 @@ export default function RootLayout({
             {children}
           </ThemeProvider>
         </NuqsAdapter>
+        <Analytics />
       </body>
     </html>
   );
